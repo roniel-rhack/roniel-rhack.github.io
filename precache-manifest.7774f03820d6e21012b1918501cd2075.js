f841f7773e1ff2a413c7cbe78d012a01a56c5aa3
Roniel <roniel.rack@gmail.com>
2020-08-07 16:42:24 -0400

    Updates

---
+++ precache-manifest.7774f03820d6e21012b1918501cd2075.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35fe0e988984ce295bab370c4d000b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09aeb052280b86bbbae3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea6d1bb458a9bb958748",
+    "url": "/static/js/2.ba1df81f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba1df81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09aeb052280b86bbbae3",
+    "url": "/static/js/main.c5c2a265.chunk.js"
+  },
+  {
+    "revision": "d875b6f2ec20d12f3323",
+    "url": "/static/js/runtime-main.6a1508d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
